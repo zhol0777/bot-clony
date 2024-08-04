246073985b52ce73f4994f07b7dfb1f1d83b0a00
Zhol Internet <zhol@internet.keyboard>
2024-08-04 15:15:51 -0700

    stop sending messages so much...

--- cogs/slowmode.py
+++ cogs/slowmode.py
@@ -48,7 +48,7 @@ class SlowMode(commands.Cog):
             and ctx.channel.id in HELP_CHANNEL_IDS
         ):
             await ctx.channel.edit(slowmode_delay=interval)
-            await ctx.channel.send("Slow it down!")
+            await ctx.channel.add_reaction("⏲️")
 
     @commands.command()
     @commands.has_any_role(MOD_ROLE_ID, HELPER_ROLE_ID)
